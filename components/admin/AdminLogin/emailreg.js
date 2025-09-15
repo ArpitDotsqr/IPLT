3e9dae965b4171e912ce7fc8e219baf7ebaa4ec3
Apurv-Mern <141210139+Apurv-Mern@users.noreply.github.com>
2025-09-15 12:15:28 +0530

    minor cleanup & formatting

--- components/admin/AdminLogin/emailreg.js
+++ components/admin/AdminLogin/emailreg.js
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from '@/redux/actions/auth';
-import { setCookies } from '@/components/utils';
-import { userRoles } from '@/utils/helpler';
-import { toast } from 'react-toastify';
-import LoaderComponent from '@/components/comman_component/Loader';
-
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "@/redux/actions/auth";
+import { setCookies } from "@/components/utils";
+import { userRoles } from "@/utils/helpler";
+import { toast } from "react-toastify";
+import LoaderComponent from "@/components/comman_component/Loader";
 
 const EmailRegister = (props) => {
-  const { loginDetails, setLoginDetails } = props
-  const router = useRouter()
-  const dispatch = useDispatch()
-  const [loginData, setLoginData] = useState({ password: '', showPassword: false })
+  const { loginDetails, setLoginDetails } = props;
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [loginData, setLoginData] = useState({
+    password: "",
+    showPassword: false,
+  });
   const [rememberMe, setRememberMe] = useState(false);
 
-  const loaderState = useSelector((state) => state.loginUser.isLoading)
+  const loaderState = useSelector((state) => state.loginUser.isLoading);
 
   const handleLogin = () => {
-
     if (rememberMe) {
-      localStorage.setItem('rememberMe', true);
-      localStorage.setItem('email', loginDetails.email);
+      localStorage.setItem("rememberMe", true);
+      localStorage.setItem("email", loginDetails.email);
     } else {
-      localStorage.removeItem('email');
-      localStorage.removeItem('rememberMe')
+      localStorage.removeItem("email");
+      localStorage.removeItem("rememberMe");
     }
 
     //   dispatch(login(loginDetails)).then((res) => {
@@ -47,90 +48,136 @@ const EmailRegister = (props) => {
     //     }
     //   })
     // }
-    dispatch(login(loginDetails)).then((res) => {
-      if (res.payload.success) {
-        setCookies(10, res.payload.data.token, res.payload.data.userData.Role.key, res.payload.data.userData.id)
-        localStorage.setItem("token", res.payload.data.token)
-        toast.success("Successfully Logged In")
-        let userRole = res.payload.data.userData.Role.key;
-       
-        if (userRole === userRoles.superadmin || userRole === userRoles.admin || userRole === userRoles.mis) {
+    dispatch(login(loginDetails))
+      .then((res) => {
+        if (res.payload.success) {
+          setCookies(
+            10,
+            res.payload.data.token,
+            res.payload.data.userData.Role.key,
+            res.payload.data.userData.id
+          );
+          localStorage.setItem("token", res.payload.data.token);
 
-          window.location.href = '/admin/dashboard';
+          let userRole = res.payload.data.userData.Role.key;
+
+          if (
+            userRole === userRoles.superadmin ||
+            userRole === userRoles.admin ||
+            userRole === userRoles.mis
+          ) {
+            toast.success("Successfully Logged In");
+            window.location.href = "/admin/dashboard";
+          } else {
+            toast.error("You are not authorized to access this page");
+          }
         } else {
-          // Handle other user roles here if needed
+          let errorMessage = res.payload.message.user
+            ? res.payload.message.user
+            : res.payload.message.password
+            ? res.payload.message.password
+            : "An Error Occurred";
+          toast.error(errorMessage);
         }
-      } else {
-        let errorMessage = res.payload.message.user ? res.payload.message.user : res.payload.message.password ? res.payload.message.password : "An Error Occurred";
-        toast.error(errorMessage);
-      }
-    }).catch((error) => {
-      // Handle errors here if needed
-      console.error("Error:", error);
-    });
-
-  }
+      })
+      .catch((error) => {
+        // Handle errors here if needed
+        console.error("Error:", error);
+      });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // setUserlogin(true)
-  }
+  };
 
   const togglePasswordVisibility = () => {
-    setLoginData({ ...loginData, showPassword: !loginData.showPassword })
-  }
+    setLoginData({ ...loginData, showPassword: !loginData.showPassword });
+  };
 
   useEffect(() => {
-    const storedRememberMe = localStorage.getItem('rememberMe') === true
+    const storedRememberMe = localStorage.getItem("rememberMe") === true;
     if (storedRememberMe) {
-      const storedEmail = localStorage.getItem('email')
-      setRememberMe(true)
-      setLoginDetails({ ...loginDetails, email: storedEmail })
+      const storedEmail = localStorage.getItem("email");
+      setRememberMe(true);
+      setLoginDetails({ ...loginDetails, email: storedEmail });
     }
-  }, [])
+  }, []);
 
   return (
     <>
       {loaderState && <LoaderComponent />}
       {
-        <div className='login-user_main bg_image'>
+        <div className="login-user_main bg_image">
           <Container>
-            <Row className='align-items-center'>
+            <Row className="align-items-center">
               <Col className="item_center" md={12}>
                 <div className="login_form">
-                  <div className='logo_login_main'>
+                  <div className="logo_login_main">
                     <div class="text-center">
-                      <Image width={65} height={65} src="/images/infraPrimeLogo.svg" alt="" class="img-fluid login_logo_img" />
+                      <Image
+                        width={65}
+                        height={65}
+                        src="/images/infraPrimeLogo.svg"
+                        alt=""
+                        class="img-fluid login_logo_img"
+                      />
                       <h1>InfraPrime Logistics Login</h1>
-                      <p className="mb-0">This is a secure site. Please enter your information to enter.</p>
+                      <p className="mb-0">
+                        This is a secure site. Please enter your information to
+                        enter.
+                      </p>
                     </div>
 
-                    <div className='login_form_main'>
+                    <div className="login_form_main">
                       <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label>Email Id</Form.Label>
-                          <Form.Control type="email"
+                          <Form.Control
+                            type="email"
                             placeholder="Enter email id"
                             value={loginDetails.email}
-                            onChange={(e) => setLoginDetails({ ...loginDetails, email: e.target.value })}
+                            onChange={(e) =>
+                              setLoginDetails({
+                                ...loginDetails,
+                                email: e.target.value,
+                              })
+                            }
                           />
-                          <Form.Text className="text-muted">
-                          </Form.Text>
+                          <Form.Text className="text-muted"></Form.Text>
                         </Form.Group>
 
-                        <Form.Group className="mb-3 position-relative" controlId="formBasicPassword">
+                        <Form.Group
+                          className="mb-3 position-relative"
+                          controlId="formBasicPassword">
                           <Form.Label>Password</Form.Label>
-                          <Form.Control type={loginData.showPassword ? 'text' : "password"}
+                          <Form.Control
+                            type={loginData.showPassword ? "text" : "password"}
                             placeholder="Enter Password"
                             value={loginDetails.password}
-                            onChange={(e) => setLoginDetails({ ...loginDetails, password: e.target.value })}
+                            onChange={(e) =>
+                              setLoginDetails({
+                                ...loginDetails,
+                                password: e.target.value,
+                              })
+                            }
                           />
-                          <span className='password_visiable position-absolute' onClick={togglePasswordVisibility}>
-                            <Image width={20} height={30} src="/images/visibility_icon.svg" alt="" class="img-fluid" />
+                          <span
+                            className="password_visiable position-absolute"
+                            onClick={togglePasswordVisibility}>
+                            <Image
+                              width={20}
+                              height={30}
+                              src="/images/visibility_icon.svg"
+                              alt=""
+                              class="img-fluid"
+                            />
                           </span>
                         </Form.Group>
-                        <Form.Group className="remember_pass d-flex justify-content-between" controlId="formBasicCheckbox">
-                          <div className='d-flex '>
+                        <Form.Group
+                          className="remember_pass d-flex justify-content-between"
+                          controlId="formBasicCheckbox">
+                          <div className="d-flex ">
                             {/* <Form.Check type="checkbox" className='d-flex align-items-center ' />
                           <label className='d-flex align-items-center ps-2'>Remember me</label> */}
 
@@ -139,26 +186,32 @@ const EmailRegister = (props) => {
                               className=""
                               id="rememberMe"
                               name="rememberMe"
-                              checked={rememberMe || localStorage.getItem('email') !== null}
+                              checked={
+                                rememberMe ||
+                                localStorage.getItem("email") !== null
+                              }
                               onChange={(event) =>
                                 setRememberMe(event.target.checked)
                               }
                             />
                             <label
-                              className='d-flex align-items-center ps-2'
-                              htmlFor="rememberMe"
-                            >
+                              className="d-flex align-items-center ps-2"
+                              htmlFor="rememberMe">
                               Remember me
                             </label>
-
                           </div>
-                          <span className='forgot_password' style={{ 'cursor': 'pointer' }} onClick={() => router.push('/forgotpassword')}>Forgot Password?</span>
+                          <span
+                            className="forgot_password"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => router.push("/forgotpassword")}>
+                            Forgot Password?
+                          </span>
                         </Form.Group>
-                        <Button variant="primary"
+                        <Button
+                          variant="primary"
                           type="submit"
-                          className='w-100 form_login_btn mb-0'
-                          onClick={handleLogin}
-                        >
+                          className="w-100 form_login_btn mb-0"
+                          onClick={handleLogin}>
                           Login here
                         </Button>
                       </Form>
@@ -172,12 +225,11 @@ const EmailRegister = (props) => {
                 </div>
               </Col> */}
             </Row>
-
           </Container>
         </div>
       }
     </>
-  )
-}
+  );
+};
 
-export default EmailRegister
+export default EmailRegister;
--- components/admin/AdminLogin/index.js
+++ components/admin/AdminLogin/index.js
@@ -11,110 +11,125 @@ import { toast } from "react-toastify";
 import ChangePassword from "./changepassword";
 import LoaderComponent from "@/components/comman_component/Loader";
 
-
 const AdminLogin = () => {
-    const router = useRouter()
-    const dispatch = useDispatch()
-    const [loginDetails, setLoginDetails] = useState({ email: "", password: "" });
-    const [defaultPage, setDefaultPage] = useState(0)
-    const [emailError, setEmailError] = useState('')
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [loginDetails, setLoginDetails] = useState({ email: "", password: "" });
+  const [defaultPage, setDefaultPage] = useState(0);
+  const [emailError, setEmailError] = useState("");
 
-    const LoaderEmailVerify = useSelector((state) => state.userSlice.isLoading)
+  const LoaderEmailVerify = useSelector((state) => state.userSlice.isLoading);
 
-    const validateForm = () => {
-        if (!loginDetails.email) {
-            setEmailError(toast.info('Please enter your email'))
-            return false
-        }
-        setEmailError('')
-        return true
+  const validateForm = () => {
+    if (!loginDetails.email) {
+      setEmailError(toast.info("Please enter your email"));
+      return false;
     }
+    setEmailError("");
+    return true;
+  };
 
-    const handleSubmit = (e) => {
-        debugger
-        e.preventDefault()
+  const handleSubmit = (e) => {
+    // debugger
+    e.preventDefault();
 
-        if (validateForm()) {
-           
-            dispatch(verifyEmail({ email: loginDetails.email })).then((res) => {
-                 debugger;
-                if (res?.payload?.success && res?.payload?.isFindUser) {
-                    setDefaultPage(1)
-                } else if (res?.payload?.success === false && res?.payload?.isFindUser === false) {
-                    toast.info(res?.payload?.message)
-                }
-                else if (res?.payload?.success === false) {
-                    toast.info(res?.payload?.message)
-                } else if (res?.payload?.success && res?.payload?.isFindUser === false) {
-                    // setDefaultPage(2)
-                    toast.info("Please, check your email")
-                } else if (res?.payload?.success) {
-                    toast.info(res?.payload?.message)
-                }
-            })
+    if (validateForm()) {
+      dispatch(verifyEmail({ email: loginDetails.email })).then((res) => {
+        //  debugger;
+        if (res?.payload?.success && res?.payload?.isFindUser) {
+          setDefaultPage(1);
+        } else if (
+          res?.payload?.success === false &&
+          res?.payload?.isFindUser === false
+        ) {
+          toast.info(res?.payload?.message);
+        } else if (res?.payload?.success === false) {
+          toast.info(res?.payload?.message);
+        } else if (
+          res?.payload?.success &&
+          res?.payload?.isFindUser === false
+        ) {
+          // setDefaultPage(2)
+          toast.info("Please, check your email");
+        } else if (res?.payload?.success) {
+          toast.info(res?.payload?.message);
         }
+      });
     }
+  };
 
+  return (
+    <>
+      {/* start page - first  register with email only  */}
+      {LoaderEmailVerify && <LoaderComponent />}
+      {defaultPage === 0 && (
+        <div className="login-user_main bg_image">
+          <Container>
+            <Row className="align-items-center">
+              <Col className="item_center" md={12}>
+                <div className="login_form">
+                  <div className="logo_login_main">
+                    <div class="text-center">
+                      <Image
+                        width={65}
+                        height={65}
+                        src="/images/infraPrimeLogo.svg"
+                        alt=""
+                        class="img-fluid login_logo_img"
+                      />
+                      <h1>InfraPrime Logistics Login</h1>
+                      <p className="mb-0">
+                        This is a secure site. Please enter your information.
+                        </p>
+                    </div>
 
-    return (
-        <>
-            {/* start page - first  register with email only  */}
-            {LoaderEmailVerify && <LoaderComponent />}
-            {defaultPage === 0 && (
-                <div className="login-user_main bg_image">
-                    <Container>
-                        <Row className="align-items-center">
-
-                            <Col className="item_center" md={12}>
-                                <div className="login_form">
-                                    <div className="logo_login_main">
-                                        <div class="text-center">
-                                            <Image width={65} height={65} src="/images/infraPrimeLogo.svg" alt="" class="img-fluid login_logo_img" />
-                                            <h1>InfraPrime Logistics Login</h1>
-                                            <p className="mb-0">
-                                                This is a secure site. Please enter your information.
-                                            </p>
-                                        </div>
-
-                                        <div className="login_form_main">
-                                            <Form onSubmit={handleSubmit} >
-                                                <Form.Group className="mb-0" controlId="formBasicEmail">
-                                                    <Form.Label>Email ID</Form.Label>
-                                                    <Form.Control type="email"
-                                                        value={loginDetails.email}
-                                                        onChange={(e) => {
-                                                            setLoginDetails({ ...loginDetails, email: e.target.value })
-                                                        }}
-                                                        placeholder="Enter Email ID" />
-                                                    {/* <Form.Text className="text-muted">{emailError}</Form.Text> */}
-                                                </Form.Group>
-                                                <Button
-                                                    variant="primary"
-                                                    type="submit"
-                                                    className="w-100 form_login_btn mb-0"
-                                                >
-                                                    Next
-                                                </Button>
-                                            </Form>
-                                        </div>
-                                    </div>
-                                </div>
-                                {/* <div className="left_side_layer"> */}
-                                {/* <img src="/images/login_bg.jpg" /> */}
-                                {/* <img className="" src="/images/bg-road.svg" /> */}
-                                {/* </div> */}
-                            </Col>
-                        </Row>
-                    </Container>
+                    <div className="login_form_main">
+                      <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-0" controlId="formBasicEmail">
+                          <Form.Label>Email ID</Form.Label>
+                          <Form.Control
+                            type="email"
+                            value={loginDetails.email}
+                            onChange={(e) => {
+                              setLoginDetails({
+                                ...loginDetails,
+                                email: e.target.value,
+                              });
+                            }}
+                            placeholder="Enter Email ID"
+                          />
+                          {/* <Form.Text className="text-muted">{emailError}</Form.Text> */}
+                        </Form.Group>
+                        <Button
+                          variant="primary"
+                          type="submit"
+                          className="w-100 form_login_btn mb-0">
+                          Next
+                        </Button>
+                      </Form>
+                    </div>
+                  </div>
                 </div>
-            )}
+                {/* <div className="left_side_layer"> */}
+                {/* <img src="/images/login_bg.jpg" /> */}
+                {/* <img className="" src="/images/bg-road.svg" /> */}
+                {/* </div> */}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )}
 
-            {/* second page - User will enter email and email generated password */}
-            {defaultPage === 1 && <EmailRegister setLoginDetails={setLoginDetails} loginDetails={loginDetails} />}
-            {/* {defaultPage === 2 && <ChangePassword />} */}
-
-        </>
-    );
+      {/* second page - User will enter email and email generated password */}
+      {defaultPage === 1 && (
+        <EmailRegister
+          setLoginDetails={setLoginDetails}
+          loginDetails={loginDetails}
+        />
+      )}
+      {/* {defaultPage === 2 && <ChangePassword />} */}
+    </>
+  );
 };
 
 export default AdminLogin;
