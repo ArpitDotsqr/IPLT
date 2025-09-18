d7bd2ec33ad2089da9aa5fe4d9248a5c3db84786
Apurv-Mern <141210139+Apurv-Mern@users.noreply.github.com>
2025-09-18 11:54:15 +0530

    refactor: enhance code readability in Pagination component

--- components/admin/Pagination/index.js
+++ components/admin/Pagination/index.js
@@ -1,44 +1,76 @@
-import Image from 'next/image'
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import previousArrow from '../../../public/images/arrow-left.svg'
-import forwardArrow from '../../../public/images/arrow-right.svg'
+import Image from "next/image";
+import React from "react";
+import { Button } from "react-bootstrap";
+import previousArrow from "../../../public/images/Arrow-left.svg";
+import forwardArrow from "../../../public/images/Arrow-right.svg";
 
 function Pagination({ pagination, list, setPagination, count }) {
-    let listCount = count ? count : list?.count
-    const handlePagination = (e) => {
-        let page = {
-            ...pagination
-        }
-        if (e.target.name === 'previous') {
-            page.pageNo = pagination.pageNo - 1
-        } if (e.target.name === "next") {
-            page.pageNo = pagination.pageNo + 1
-        }
-        setPagination(page)
+  let listCount = count ? count : list?.count;
+  const handlePagination = (e) => {
+    let page = {
+      ...pagination,
+    };
+    if (e.target.name === "previous") {
+      page.pageNo = pagination.pageNo - 1;
     }
-    return (
-        <div className='item_justify mb-3'>
-            <div className="table_footer_start">
-                <h6>Showing {((pagination.pageNo - 1) * pagination.pageSize) + 1} - {(pagination.pageNo * pagination.pageSize) < listCount ? (pagination.pageNo * pagination.pageSize) : listCount} profiles of {listCount} profiles</h6>
-            </div>
-            <div className="">
-                {pagination.pageNo !== 1 && (
-                    <Button style={{ color: "#0061F6", background: "transparent", border: "none", fontSize: "14px", fontWeight: "500" }} className=" me-3" name='previous' onClick={(e) => handlePagination(e)}>
-                        <Image width={20} height={20} className='me-2 mb-1' src={previousArrow} name='previous' />
-                        Previous
-                    </Button>
-                )}
-                {(pagination.pageNo * pagination.pageSize) < listCount && (
-                    <Button style={{ fontSize: "14px", fontWeight: "500" }} className="border_btn green" name="next" onClick={(e) => handlePagination(e)} >
-                        Next
-                        <Image width={20} height={20} className='ms-2' src={forwardArrow} name="next" />
-
-                    </Button>
-                )}
-            </div>
-        </div>
-    )
+    if (e.target.name === "next") {
+      page.pageNo = pagination.pageNo + 1;
+    }
+    setPagination(page);
+  };
+  return (
+    <div className="item_justify mb-3">
+      <div className="table_footer_start">
+        <h6>
+          Showing {(pagination.pageNo - 1) * pagination.pageSize + 1} -{" "}
+          {pagination.pageNo * pagination.pageSize < listCount
+            ? pagination.pageNo * pagination.pageSize
+            : listCount}{" "}
+          profiles of {listCount} profiles
+        </h6>
+      </div>
+      <div className="">
+        {pagination.pageNo !== 1 && (
+          <Button
+            style={{
+              color: "#0061F6",
+              background: "transparent",
+              border: "none",
+              fontSize: "14px",
+              fontWeight: "500",
+            }}
+            className=" me-3"
+            name="previous"
+            onClick={(e) => handlePagination(e)}>
+            <Image
+              width={20}
+              height={20}
+              className="me-2 mb-1"
+              src={previousArrow}
+              name="previous"
+            />
+            Previous
+          </Button>
+        )}
+        {pagination.pageNo * pagination.pageSize < listCount && (
+          <Button
+            style={{ fontSize: "14px", fontWeight: "500" }}
+            className="border_btn green"
+            name="next"
+            onClick={(e) => handlePagination(e)}>
+            Next
+            <Image
+              width={20}
+              height={20}
+              className="ms-2"
+              src={forwardArrow}
+              name="next"
+            />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
